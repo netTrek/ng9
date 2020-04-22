86d53ca5a32fa29bc0868b879c6c0bfcce7848f2
Saban Uenlue - netTrek <us@netTrek.de>
2020-04-22 14:28:32 +0200

    userList with select, search, add and del

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -7,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
 } )
 export class UserListComponent implements OnInit {
 
-  userList: string[] = [
+  filtered: string[];
+  selectedUser: string;
+
+  private filterStr = '';
+  private userList: string[] = [
     'saban',
     'heike',
     'peter'
   ];
-
   constructor() {
   }
 
@@ -20,6 +23,40 @@ export class UserListComponent implements OnInit {
     // for ( const user of this.userList ) {
     //   console.log ( user );
     // }
+    this.filtered = [...this.userList];
+  }
+
+  setAsSelected( user: string ) {
+    // if ( this.selectedUser !== user) {
+    //   this.selectedUser = user;
+    // } else {
+    //   this.selectedUser = undefined;
+    // }
+    this.selectedUser = this.selectedUser === user ? undefined : user;
+  }
+
+  addRndUser() {
+    this.userList.push(
+      ['frank', 'paula', 'heiko', 'hanna'][Math.round( Math.random() * 3)]
+    );
+    this.updateFilter();
+  }
+
+  del() {
+    this.userList = this.userList.filter( value => value !== this.selectedUser );
+    this.selectedUser = undefined;
+    this.updateFilter();
+  }
+
+  filter( chgEvent: Event ) {
+    this.filterStr = (chgEvent.target as HTMLInputElement)
+      .value;
+    this.updateFilter();
+  }
+
+  private updateFilter() {
+      this.filtered =
+        this.userList.filter( value => value.indexOf( this.filterStr ) !== -1 );
   }
 
 }
