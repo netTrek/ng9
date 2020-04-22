649414c0fd130f180eaf84033a2b435745a1d532
Saban Uenlue - netTrek <us@netTrek.de>
2020-04-22 12:40:33 +0200

    for of first contact

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'pl-user-list',
+@Component ( {
+  selector   : 'pl-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
-})
+  styleUrls  : ['./user-list.component.scss']
+} )
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  userList: string[] = [
+    'saban',
+    'heike',
+    'peter'
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    // for ( const user of this.userList ) {
+    //   console.log ( user );
+    // }
   }
 
 }
