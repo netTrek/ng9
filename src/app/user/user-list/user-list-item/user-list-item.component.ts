a8600e358af5007424688f16f47a8ba5a7395973
Saban Uenlue - netTrek <us@netTrek.de>
2020-04-22 16:19:08 +0200

    List Item with Input And Output

--- src/app/user/user-list/user-list-item/user-list-item.component.ts
+++ src/app/user/user-list/user-list-item/user-list-item.component.ts
@@ -1,15 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostBinding, HostListener, Input, OnInit, Output } from '@angular/core';
 import { User } from '../../user';
 
-@Component({
-  selector: 'pl-user-list-item',
+@Component ( {
+  selector   : 'pl-user-list-item',
   templateUrl: './user-list-item.component.html',
-  styleUrls: ['./user-list-item.component.scss']
-})
+  styleUrls  : ['./user-list-item.component.scss']
+} )
 export class UserListItemComponent implements OnInit {
-  @Input() user: User;
-  constructor() { }
-  ngOnInit(): void {
+
+  @Input () user: User;
+  @Output() selectUsr: EventEmitter<User> = new EventEmitter<User>();
+  // [class.selected]="true" --- so haben wir es in der Eltern Komp gebunden
+  @HostBinding('class.selected')
+  @Input ()
+  selected = false;
+  constructor() {
   }
 
+  ngOnInit(): void {
+  }
+  @HostListener('click')
+  triggerSelectEvent() {
+    this.selectUsr.emit( this.user );
+  }
 }
