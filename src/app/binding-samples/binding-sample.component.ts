f255f6ad238409809abdf36c82efcfb76b1d6dd5
Saban Uenlue - netTrek <us@netTrek.de>
2020-04-22 10:14:14 +0200

    ausdruck bindung

--- src/app/binding-samples/binding-sample.component.ts
+++ src/app/binding-samples/binding-sample.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./binding-sample.component.scss']
 })
 export class BindingSampleComponent implements OnInit {
+  myString = 'saban';
+  myNum =  123;
+  myObj = { username: 'saban', userlastname: 'ünlü' };
+  imgFile = 'cat1.jpeg';
+  vorname = 'saban';
+  nachnamen = 'ünlü';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getString(prefix?: string|number ) {
+    return prefix ? `${prefix} ${this.myString}` : this.myString;
+  }
+
+  add() {
+    this.myNum ++;
+    this.imgFile = 'cat2.jpeg';
+  }
 }
