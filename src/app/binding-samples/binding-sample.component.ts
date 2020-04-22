c2aa8b990551bb3680e619080867577c4108270b
Saban Uenlue - netTrek <us@netTrek.de>
2020-04-22 09:40:42 +0200

    prepair for bindings

---
+++ src/app/binding-samples/binding-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pl-binding-sample',
+  templateUrl: './binding-sample.component.html',
+  styleUrls: ['./binding-sample.component.scss']
+})
+export class BindingSampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
