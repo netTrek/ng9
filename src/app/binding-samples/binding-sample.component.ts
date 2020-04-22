b49bf8c7147e0e44c144efe0c0904dcd5c1b440f
Saban Uenlue - netTrek <us@netTrek.de>
2020-04-22 10:39:42 +0200

    eigenschaften und attribute binden

--- src/app/binding-samples/binding-sample.component.ts
+++ src/app/binding-samples/binding-sample.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'pl-binding-sample',
+@Component ( {
+  selector   : 'pl-binding-sample',
   templateUrl: './binding-sample.component.html',
-  styleUrls: ['./binding-sample.component.scss']
-})
+  styleUrls  : ['./binding-sample.component.scss']
+} )
 export class BindingSampleComponent implements OnInit {
-  myString = 'saban';
-  myNum =  123;
-  myObj = { username: 'saban', userlastname: 'ünlü' };
-  imgFile = 'cat1.jpeg';
-  vorname = 'saban';
+  myString  = 'saban';
+  myNum     = 123;
+  myObj     = { username: 'saban', userlastname: 'ünlü' };
+  imgFile   = 'cat1.jpeg';
+  vorname   = 'saban';
   nachnamen = 'ünlü';
+  imgPath   = './assets/images/cat1.jpeg';
+  // classList = 'font-red font-bold';
+  classList = 'font-red';
+  showRed = false;
+  width = 60;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-  getString(prefix?: string|number ) {
+  getString( prefix?: string | number ) {
     return prefix ? `${prefix} ${this.myString}` : this.myString;
   }
 
   add() {
     this.myNum ++;
     this.imgFile = 'cat2.jpeg';
+    this.imgPath = './assets/images/cat2.jpeg';
+    this.showRed = !this.showRed;
+    this.width -= 5;
   }
 }
